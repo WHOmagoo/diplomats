defc3650a076ceabfd0b8e31e94cf1fa962f09cb
Connor Wright <connor.d.wright@wsu.edu>
2017-10-16 22:04:34 -0400

    finished base implementation of post requests will need to be fleshed out in later iterations

--- GameEngine.py
+++ GameEngine.py
@@ -6,14 +6,33 @@ app = Flask(__name__)
 
 defUrl = '/api/'
 
-@app.route('/')
-def test():
-    return 'test'
-
+#recives and validates order
+#order is of the form JSON: {“unitID”= int,”targetName”=string, “orderType”=string }
 @app.route(defUrl + 'send_order', methods=['POST'])
 def send_order():
     if request.is_json:
+        #content will contain a dictionary describing the order
         content = request.get_json()
-        return content['username']
+        #validate_order will return true if the order is valid
+        #valid = validate_order(content)
+        if valid:
+            #order is ok
+            return 200
+        else:
+            #order is not ok
+            return 418
+    else:
+        return 418
+
+#recives a faction name in a json and returns 200 when all other players
+#have confirmed orders
+#json of the form {"faction"="faction_name"}
+@app.route(defUrl + 'confirm_orders', methods=['POST'])
+def confirm_orders():
+    if request.is_json:
+        content = request.get_json()
+        #waits until all orders are in then returns
+        #confirm(content)
+        return 200
     else:
         return 418
